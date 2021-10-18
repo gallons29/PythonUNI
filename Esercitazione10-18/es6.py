3535cad40ee14fa7910b3ef04ae12d5396d54759
Gallons Hp <alegalloni28@gmail.com>
2021-10-18 14:25:37 +0200

    inizio 6

---
+++ Esercitazione10-18/es6.py
@@ -0,0 +1,9 @@
+def maiuscolo_asterischi(str):
+    for char in str:
+        if()
+    
+def main():
+    user_text = input("Inserisci un testo: ")
+    text_maiuscolo = maiuscolo_asterischi(user_text)
+    print(text_maiuscolo)
+main()
