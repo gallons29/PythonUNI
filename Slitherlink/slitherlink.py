2b5ee01b119ed480310b76fbad64d04c1bf8dba6
Alessandro <alegalloni28@gmail.com>
2021-12-01 15:37:36 +0100

    Lettura board da file. Commenti

--- Slitherlink/slitherlink.py
+++ Slitherlink/slitherlink.py
@@ -9,10 +9,17 @@ class Slitherlink(BoardGame):
         self._board = board
 
     def play_at(self, x: int, y: int):
+        '''
+        In x pari, y pari: si trovano i segni +
+        In x dispari, y dispari: si trovano i numeri (se presenti)
+        Le linee vanno per cui messe nei casi in cui:
+            - x dispari, y pari: linea orizzontale -
+            - x pari, y dispari: linea verticale |
+        '''
         if x % 2 == 0 and y % 2 != 0:
-            if self._board[y * self._cols + x] == ' ':
+            if self._board[y * self._cols + x] == ' ': #se lo "slot" è vuoto, viene riempito con la linea,
                 self._board[y * self._cols + x] = '|'
-            else:
+            else:                                      #altrimenti (caso "slot" già riempito dalla linea), rimuove la linea
                 self._board[y * self._cols + x] = ' '
         elif x % 2 != 0 and y %2 == 0:
             if self._board[y * self._cols + x] == ' ':
@@ -37,16 +44,16 @@ class Slitherlink(BoardGame):
     def message(self) -> str:
         pass
 
-board = list("+ + + + + +" +
-             "   3 2 2   " +
-             "+ + + + + +" +
-             "   0     2 " +
-             "+ + + + + +" +
-             "   2     1 " +
-             "+ + + + + +" +
-             "   0     2 " +
-             "+ + + + + +" +
-             "   2   2   " +
-             "+ + + + + +")
+
+# -------------------------------- #
+# Lettura board da file:           #
+with open("Slitherlink\game_5x5.txt", "r") as b:
+    board = []
+    for line in b:
+        for char in line:
+            if char != "\n":
+                board.append(char)
+# -------------------------------- #
+
 
 gui_play(Slitherlink(11, 11, board))
